558c065a7eda835d9272fafcb605ca79f7f4337b
Collin R Wilson <CollinRWilson@gmail.com>
2017-07-21 19:02:27 -0400

    Updated for PEP8

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
-def LoadPickle():
+def loadpickle():
     import pickle
     f = open("pickle-data", "rb")
     return_data = pickle.load(f)
     f.close()
     return return_data
 
-def SavePickle(to_do_list):
+
+def savepickle(to_do_list):
     import pickle
-    f = open("pickle-data","wb")
-    pickle.dump(to_do_list,f)
+    f = open("pickle-data", "wb")
+    pickle.dump(to_do_list, f)
     f.close()
 
-def RemoveIndex(to_do_list):
-    for list_num in range(0,len(to_do_list)):
-        print("{}. {}".format(list_num,to_do_list[list_num]))
+
+def removeindex(to_do_list):
+    for list_num in range(0, len(to_do_list)):
+        print("{}. {}".format(list_num, to_do_list[list_num]))
     input_number = input("Enter the number to delete or Exit to leave.\n")
     if input_number == "Exit":
         return to_do_list
@@ -24,29 +26,30 @@ def RemoveIndex(to_do_list):
         del to_do_list[int(input_number)]
         return to_do_list
 
+
 def main():
     from pathlib import Path
     to_do_list = []
     my_file = Path("pickle-data")
     if my_file.is_file():
-        to_do_list = LoadPickle()
+        to_do_list = loadpickle()
     exit_time = False
     while not exit_time:
         prompt = input("Please enter what to do. Read/Write/Remove/Exit\n")
         if prompt == "Read":
             if my_file.is_file():
-                to_do_list = LoadPickle()
+                to_do_list = loadpickle()
                 for line in to_do_list:
                     print("{}".format(line))
             else:
                 print("No data in file.")
         elif prompt == "Write":
             to_do_list.append(input("Please enter to do: \n"))
-            SavePickle(to_do_list)
+            savepickle(to_do_list)
         elif prompt == "Remove":
             if my_file.is_file():
-                to_do_list = RemoveIndex(to_do_list)
-                SavePickle(to_do_list)
+                to_do_list = removeindex(to_do_list)
+                savepickle(to_do_list)
             else:
                 print("No data in file.")
         elif prompt == "Exit":
@@ -55,5 +58,5 @@ def main():
             print("Request not understood. Please try again.")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
